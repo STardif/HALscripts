e909a70a95ff5c69eb6f49b6794a8ef575d6fae6
Sam Tardif <STardif@users.noreply.github.com>
2019-03-04 15:45:28 +0100

    initial commit

---
+++ doublons_hal_check.py
@@ -0,0 +1,181 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 15:24:41 2019
+
+@author: st242386 (samuel.tardif@cea.fr)
+@date: 2018-02-13
+ 
+The purpose of the script is to identify possible doubles in the list of 
+publications from HAL for a given laboratory.
+
+It gets the list of publications from a http request to the haltools server and
+compares every possible pair by (1) title fuzzy matching and then (2) by 
+comparing the DOI (if any)
+
+The parameters for getting the publications list are (see top paragraph):
+    annee_publideb : year  (e.g. '2014')
+    annee_publifin : year  (e.g. '2050')
+    struct : code for the laboratory (e.g. '40368' for MEM/NRS)
+
+No warranties are made on the accuracy or exhaustivity of the results !
+"""
+
+
+###############################################
+# EDIT HERE
+# Publications Search Parameters
+params = {'annee_publideb' : 2014,
+          'annee_publifin' : 2050,
+          'struct' : 40368}
+
+# Title correlation threshold (100 = full match, 0 = no match)
+title_correlation_threshold = 85 
+###############################################
+
+
+
+# BELOW IS THE ACTUAL SCRIPT
+
+# Imports
+import urllib
+from bs4 import BeautifulSoup
+from numpy import arange
+from fuzzywuzzy import fuzz
+
+# quick formatting, make sure all is in string format
+for p in params: params[p] = str(params[p])
+
+# additional parameters
+add_params = {'typdoc' : '(%27ART%27,%27OUV%27,%27COUV%27,%27DOUV%27,%27COMM%27,'+\
+                              '%27PATENT%27,%27OTHER%27,%27UNDEFINED%27,%27THESE%27,'+\
+                              '%27HDR%27)',
+              'CB_auteur' : 'oui',
+              'CB_titre' : 'oui',
+              'CB_article' : 'oui',
+              'CB_DOI' : 'oui',
+              'langue' : 'all',
+              'tri_exp' : 'titre',
+              'ordre_aff' : 'TA',
+              'CB_rubriqueDiv' : 'oui',
+              'Fen' : 'Aff'}
+
+
+#build the url for the request
+link = "http://haltools.archives-ouvertes.fr/Public/afficheRequetePubli.php?"
+link = '&'.join([link]+['{}={}'.format(p,params[p]) for p in params])
+link = '&'.join([link]+['{}={}'.format(p,add_params[p]) for p in add_params])
+print("using this url for the request :")
+print(link)
+print('\n')
+
+
+# get the list from haltools and parse it 
+html = urllib.request.urlopen(link).read()
+ph = BeautifulSoup(html, features = "lxml") # parsed html
+refs = ph.find_all('dl') # list of all references
+print("Found {:} references".format(len(refs)))
+print('\n')
+
+print("Matching title threshold = {:}%".format(title_correlation_threshold))
+print('\n')
+
+
+
+def get_doi(ref):
+    """
+    Extract the DOI for a given reference
+    since there is no DOI field, I parse the dx.doi.org url (if any!)
+    NOTE: Might be a possible confusion if there are supplementary materials with
+    their own DOI ?? 
+    
+    input : 
+        ref : dl tag 
+    """
+    doi = None
+    # if the DOI field is present
+    if 'CB_DOI' in add_params.keys():
+        for dd in ref.find_all('dd'):
+            if len(dd.contents[0]) > 3:
+                if dd.contents[0][:3] == 'DOI':
+                    doi = dd.contents[1].contents[0].lower()
+    else:
+        # brute force : list all urls in the tag and find the first matching one
+        for atag in ref.find_all('a'):
+            if atag['href'].startswith('https://dx.doi.org/'):
+                doi = atag['href'][len('https://dx.doi.org/'):].lower()
+                break
+    return doi
+
+
+
+def print_double(ref1, ref2):
+    """
+    Do the display part for matching references
+    
+    input : 
+        ref1 : dl tag 
+        ref2 : dl tag 
+    """
+    print('ref {:>3} : '.format(iii), ref1.dd.string)
+    print('ref {:>3} : '.format(jjj), ref2.dd.string)
+    print('correlation: ', correl)
+    print('DOI 1 : ', get_doi(ref1))
+    print('url originale: ', (ref1.a)['href'])
+    print('url hal:', (ref1.a)['href'].replace('hal.univ-grenoble-alpes.fr',
+                                               'hal.archives-ouvertes.fr'))
+    print('DOI 2: ', get_doi(ref2))
+    print('url originale: ', (ref2.a)['href'])
+    print('url hal:', (ref2.a)['href'].replace('hal.univ-grenoble-alpes.fr',
+        'hal.archives-ouvertes.fr').replace('hal-cea','hal'))
+    print('\n')
+	
+	
+	
+# counters for the statistics
+doi_double = 0
+missing_doi_double = 0
+title_double = 0
+
+
+# loop over all refs and check matching titles
+for iii in arange(len(refs)-1):
+    doi1 = get_doi(refs[iii])            
+    for jjj in arange(iii+1, len(refs)):
+        # compute the matching rate
+        correl = fuzz.ratio(refs[iii].dd.string, refs[jjj].dd.string)
+        if correl > title_correlation_threshold :
+            doi2 = get_doi(refs[jjj])                
+            
+            # DOI match = obvious double
+            if doi1 != None and doi2 != None and doi2 == doi1:
+                print('-'*20)
+                print('!!!! DOUBLE FOUND (DOI match) !!!!')
+                print_double(refs[iii], refs[jjj])
+                doi_double += 1
+                
+            # only title match = possible double
+            # not matching doi
+            elif doi1 != None and doi2 != None and doi2 != doi1:
+                print('-'*20)
+                print('! Possible match (but different doi) !')
+                print_double(refs[iii], refs[jjj]) 
+                title_double += 1               
+                
+            # only title match = possible double
+            # missing doi
+            elif doi1 == None or doi2 == None:
+                print('-'*20)
+                print('!!! Possible match (missing DOI to confirm) !!!')
+                print_double(refs[iii], refs[jjj])
+                missing_doi_double += 1
+            
+print('#'*20)
+print('found {:} DOI doubles'.format(doi_double))
+print('found {:} possible doubles (no DOI to confirm)'.format(missing_doi_double))
+print('found {:} title doubles but with different DOI'.format(title_double))
+
+        
+            
+
+            
---
+++ lab_connections.py
@@ -0,0 +1,149 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 15:24:41 2019
+
+@author: st242386 (samuel.tardif@cea.fr)
+@date: 2018-02-13
+ 
+The purpose of the script is to ...
+"""
+"""
+labs = {'MEM' : 460264,
+        'LETI' : 40214,
+        'LITEN' : 40221}
+
+"""      
+labs = {'NRS' : 40368,
+        'LSim' : 40364,
+        'MDN' : 40365,
+        'LEMMA' : 40362,
+        'SGX' : 460267,
+        'RM' : 40372}
+
+annee_publideb = 2014
+annee_publifin = 2019
+
+
+# Imports
+import urllib
+from bs4 import BeautifulSoup
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_ref(struct):
+
+    # Publications Search Parameters
+    params = {'annee_publideb' : annee_publideb,
+              'annee_publifin' : annee_publifin,
+              'struct' : struct}
+    
+    # quick formatting, make sure all is in string format
+    for p in params: params[p] = str(params[p])
+    
+    # additional parameters
+    add_params = {'typdoc' : '(%27ART%27,%27OUV%27,%27COUV%27,%27DOUV%27,%27COMM%27,'+\
+                                  '%27PATENT%27,%27OTHER%27,%27UNDEFINED%27,%27THESE%27,'+\
+                                  '%27HDR%27)',
+                  'CB_auteur' : 'oui',
+                  'CB_titre' : 'oui',
+                  'CB_article' : 'oui',
+                  'CB_DOI' : 'oui',
+                  'langue' : 'all',
+                  'tri_exp' : 'titre',
+                  'ordre_aff' : 'TA',
+                  'CB_rubriqueDiv' : 'oui',
+                  'Fen' : 'Aff'}
+    
+    
+    #build the url for the request
+    link = "http://haltools.archives-ouvertes.fr/Public/afficheRequetePubli.php?"
+    link = '&'.join([link]+['{}={}'.format(p,params[p]) for p in params])
+    link = '&'.join([link]+['{}={}'.format(p,add_params[p]) for p in add_params])
+    print("using this url for the request :")
+    print(link)
+    print('\n')
+    
+    
+    # get the list from haltools and parse it 
+    html = urllib.request.urlopen(link).read()
+    ph = BeautifulSoup(html, features = "lxml") # parsed html
+    refs = ph.find_all('dl') # list of all references
+    print("Found {:} references".format(len(refs)))
+    print('\n')
+    return refs
+
+
+
+
+def get_ref_list(labs):
+    refs = {}
+    for lab in labs : refs[lab] = get_ref(labs[lab])
+    return refs
+
+
+
+def get_overlap(lab1,lab2,refs):
+    return len(set.intersection(set(refs[lab1]),set(refs[lab2])))
+
+
+def plot_figure(labs,refs,
+                radius_scale_factor = 0.03,
+                txt_offset_scale_factor = 0.1,
+                vertex_width_scale_factor = 1):
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(111)
+    #fig.patch.set_visible(False)
+    ax.set_xlim((-1.5,1.5))
+    ax.set_ylim((-1.5,1.5))
+    ax.axis('off')
+    ax.set_aspect('equal')
+    
+    nlabs = len(labs)            
+    lab_circle={}
+    ilab = 0
+    for lab in labs:
+        lab_circle[lab] = [np.cos(ilab*2*np.pi/nlabs),
+                           np.sin(ilab*2*np.pi/nlabs),
+                           np.sqrt(len(refs[lab]))*radius_scale_factor]
+        ilab+=1
+        
+        
+    done = []
+    for lab1 in labs:
+        done += [lab1]
+        x1,y1,r1 = lab_circle[lab1][0],lab_circle[lab1][1],lab_circle[lab1][2]
+        ax.add_artist(plt.Circle((x1, y1), r1, zorder=1,
+                                 facecolor = 'w',
+                                 edgecolor = 'k'))
+        ax.text(x1,y1,lab1+'\n{:}'.format(len(refs[lab1])), 
+                horizontalalignment='center',
+                verticalalignment='center')
+    
+        for lab2 in labs:
+            if lab1 != lab2 and lab2 not in done:
+                x2,y2 = lab_circle[lab2][0],lab_circle[lab2][1]
+                ol = get_overlap(lab1,lab2,refs)
+                if ol > 0:
+                    ax.plot([x1,x2],[y1,y2],lw=ol*vertex_width_scale_factor,
+                            zorder=0,color='grey')
+                    ax.text((x2+x1)/2-txt_offset_scale_factor*(y2-y1)/2,
+                            (y2+y1)/2+txt_offset_scale_factor*(x2-x1)/2,
+                            str(ol), 
+                            horizontalalignment='center',
+                            verticalalignment='center')                
+                    #ax.text((x2+x1)/2-0.1*(y2-y1)/2,
+                    #        (y2+y1)/2+0.1*(x2-x1)/2,
+                    #        str(ol))
+    
+    ax.set_title('Total and common publications '+\
+                 'between {:} and {:}'.format(annee_publideb,annee_publifin))          
+    plt.show()
+
+
+refs = get_ref_list(labs)
+plot_figure(labs,refs,
+                radius_scale_factor = 0.03,
+                txt_offset_scale_factor = 0.2,
+                vertex_width_scale_factor = 1)
